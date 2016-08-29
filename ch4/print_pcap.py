aecac6720f01f7dddf93c0f801068984635c78c1
Dmitry Dulepov <dmitry.dulepov@gmail.com>
2016-08-29 17:52:37 +0300

    [TASK] Add geoip to print pcap

--- ch4/print_pcap.py
+++ ch4/print_pcap.py
@@ -1,4 +1,5 @@
 import dpkt
+import pygeoip
 import socket
 import sys
 
@@ -16,7 +17,14 @@ except:
 	print "Unable to open %s" % (sys.argv[1])
 	exit(1)
 
+try:
+	geoIp = pygeoip.GeoIP('/opt/local/share/GeoIP/GeoLiteCity.dat')
+except:
+	print 'Cannot open GeoIP database'
+	exit(1)
+
 nsCache = {}
+geoCache = {}
 for (ts, buffer) in reader:
 	try:
 		ethPacket = dpkt.ethernet.Ethernet(buffer)
@@ -28,7 +36,22 @@ for (ts, buffer) in reader:
 				nsCache[destinationIpAddress] = socket.gethostbyaddr(destinationIpAddress)[0]
 			except:
 				nsCache[destinationIpAddress] = '?'
-		print "%s -> %s (%s)" % (sourceIpAddress, destinationIpAddress, nsCache[destinationIpAddress])
+		if destinationIpAddress not in geoCache:
+			try:
+				record = geoIp.record_by_name(destinationIpAddress)
+				geoInfo = ''
+				if record['city'] != '':
+					geoInfo = record['city']
+				if record['country_code3']:
+					if geoInfo != '':
+						geoInfo = geoInfo + ', '
+					geoInfo = geoInfo + record['country_code3']
+				if geoInfo == '':
+					geoInfo = 'no geo info'
+				geoCache[destinationIpAddress] = '(' + geoInfo + ')'
+			except:
+				geoCache[destinationIpAddress] = '(cannot resolve)'
+		print "%s -> %s (%s; %s)" % (sourceIpAddress, destinationIpAddress, geoCache[destinationIpAddress], nsCache[destinationIpAddress])
 	except:
 		print "Malformed packet?"
 
